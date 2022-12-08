15c0ce53b43ada0748fb28b22fb9f5215641fc88
Enzo Sandré <atelier@enzosandre.fr>
2022-12-08 12:55:59 +0100

    Test Bonjour avec langue et période de la journée

--- src/Langues/LangueFrancaise.py
+++ src/Langues/LangueFrancaise.py
@@ -1,10 +1,13 @@
 from Langues.Constantes import Constantes
+from PeriodeDeLaJournee import PeriodeDeLaJournee
 
 
 class LangueFrancaise:
 
     def dire_bonjour(self, periode_journee):
-        return Constantes.Francais.BONJOUR
+        return Constantes.Francais.BONSOIR \
+            if periode_journee in (PeriodeDeLaJournee.SOIR, PeriodeDeLaJournee.NUIT)\
+            else Constantes.Francais.BONJOUR
 
     def bien_dit(self):
         return Constantes.Francais.BIEN_DIT
